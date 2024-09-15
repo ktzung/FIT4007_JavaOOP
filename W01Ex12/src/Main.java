9165b0cffa4ff536c4d3a5096a090996928a033c
ktzung <dung.kt230057d@sis.hust.edu.vn>
2024-09-15 10:37:44 +0700

    Update Exercise 12 of Week 01

--- W01Ex12/src/Main.java
+++ W01Ex12/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int c = a + b;
-        System.out.println("Sum of a and b is: " + c);
+        System.out.println("a and b is: " + a + " " + b);
     }
 }
